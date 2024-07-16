f873d7c3533580c48f2abd78b96fb5672f461292
Mo Omer <momer@apache.org>
2024-07-16 14:21:13 -0500

    Fix/acceptance tests (#19)
    * Update cluster data test to create/read cluster within the same step.
    
    * Ensure at least one cluster exists during data source list acceptance test
    
    * Disable acceptance tests except for when label 'acceptance' exists

--- internal/cluster/data_source_test.go
+++ internal/cluster/data_source_test.go
@@ -3,29 +3,47 @@ package cluster_test
 import (
 	"fmt"
 
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func (s *ClusterTestSuite) TestCluster_DataSource() {
-	const clusterSlug = "dcek-group-llc-5240651189"
+	clusterSuffix := acctest.RandString(16)
+	clusterName := fmt.Sprintf("bonsai test %s", clusterSuffix)
+
 	resource.Test(s.T(), resource.TestCase{
 		ProtoV6ProviderFactories: s.ProtoV6ProviderFactories,
+		CheckDestroy:             testClusterDestroyed("bonsai_cluster.test", s.Client),
 		Steps: []resource.TestStep{
 			{
+				ResourceName: "bonsai_cluster.test",
 				Config: fmt.Sprintf(`
+						resource "bonsai_cluster" "test" {
+							name = "%s"
+
+							plan = { 
+								slug = "sandbox"
+							}
+
+							space = { 
+								path = "omc/bonsai/us-east-1/common"
+							}
+
+							release = { 
+								slug = "opensearch-2.6.0-mt"
+							}
+						}
+
 						data "bonsai_cluster" "get_by_slug" {
-						  slug = "%s"
+						  slug = resource.bonsai_cluster.test.id
 						}
 
 						output "bonsai_cluster_slug" {
 						  value = data.bonsai_cluster.get_by_slug.slug
 						}
-					`, clusterSlug),
+					`, clusterName),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.bonsai_cluster.get_by_slug", "slug"),
-					resource.TestCheckResourceAttr("data.bonsai_cluster.get_by_slug", "slug", clusterSlug),
-					resource.TestCheckResourceAttr("data.bonsai_cluster.get_by_slug", "name", "DCEK Group, LLC search"),
-					resource.TestCheckOutput("bonsai_cluster_slug", clusterSlug),
 				),
 			},
 		},
